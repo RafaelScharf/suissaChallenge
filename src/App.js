120ca822f5a4335b93b48f3c8d16b4fb0e15a7a3
rafaelScharf <rafaelkscharf@gmail.com>
2020-10-19 19:20:39 -0300

    alterei textos

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ function App() {
   return (
     <div className="App">
   
-      <h1>Ola mundo</h1>
-     
+    <h1>Hello Word</h1>
+    <p>
+      This project is a setup with ReactJS, 
+      Jest, React Library test, Storybook
+      and Atomic Design
+    </p>
+    <h2>Why use it?</h2>
+    <p>You can use this project to start a 
+      react application with automated tests 
+      already configured with JEST and React 
+      Test Library.
+      This is not the only thing that is configured 
+      for you, with it you will be able to take 
+      advantage of the Atomic Design pattern design 
+      and the best of all? It is already integrated 
+      with StoryBook!
+      </p>
+
     </div>
   );
 }
