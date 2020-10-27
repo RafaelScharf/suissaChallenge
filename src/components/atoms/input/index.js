62516827aa617c92f30d86f6801f267f3d806efd
Suissa <jnascimento@gmail.com>
2020-10-27 18:44:51 -0300

    PAI TA ON PORRRAAA
    Agora o paste ta correto

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -6,15 +6,19 @@ import {
 } from './utils';
 
 let isBackspace = false;
+let inputValue = '';
 
 const Input = () => {
 
   const [number, setNumber] = useState();
 
+  console.log({inputValue}, {number})
   const handleChangeKeyPress = evt => {
+    
+    console.log('handleChangeKeyPress', {inputValue}, {number})
     if ( evt.keyCode == 8 ) 
       isBackspace = true
-      
+    console.log('handleChangeKeyPress', {isBackspace}, evt.keyCode, evt.target.value)
     setNumber(
       (isNumberOrDot(evt))
         ? evt.target.value + evt.key
@@ -23,6 +27,8 @@ const Input = () => {
   }
 
   const handleChangeKeyUp = evt => {
+    
+    console.log('handleChangeKeyUp', {inputValue}, {number})
     if (isBackspace) return false;
     setNumber(
       (countDots(evt) == 0 || isBackspace)
@@ -32,16 +38,29 @@ const Input = () => {
   }
 
   const handleChange = evt => {
+
+    console.log('handleChange', {inputValue}, {number})
+    console.log('change:', evt.keyCode, evt.target.value)
+    console.log({isBackspace}, evt.keyCode)
     if ( isBackspace ){
       isBackspace = false;
       return setNumber(evt.target.value);
     }
 
-    setNumber(
-      (/[a-zA-Z]|,/gim.test(evt.target.value))
-        ? evt.target.value.replace(/[a-zA-Z]|,/gim, '')
-        : evt.target.value.slice(0, -1)
-    )
+    if (inputValue == "") {
+      inputValue = evt.target.value
+      setNumber(inputValue)
+      console.log('handleChange', {inputValue}, {number})
+    }
+
+    else {
+      setNumber(
+        (/[a-zA-Z]|,/gim.test(evt.target.value))
+          ? evt.target.value.replace(/[a-zA-Z]|,/gim, '')
+          : evt.target.value.slice(0, -1)
+      )
+
+    }
   }
   
   
