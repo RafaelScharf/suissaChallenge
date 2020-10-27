7d9a2d78d4a3fa3e0b2579761440981b5bcb24fb
Suissa <jnascimento@gmail.com>
2020-10-27 20:55:41 -0300

    RESOLVIDO SEUS ARROMBADOS!!!

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -6,16 +6,12 @@ import {
 } from './utils';
 
 let isBackspace = false;
-let inputValue = '';
 
 const Input = () => {
 
   const [number, setNumber] = useState();
 
-  console.log({inputValue}, {number})
   const handleChangeKeyPress = evt => {
-    
-    console.log('handleChangeKeyPress', {inputValue}, {number})
     if ( evt.keyCode == 8 ) 
       isBackspace = true
     console.log('handleChangeKeyPress', {isBackspace}, evt.keyCode, evt.target.value)
@@ -26,42 +22,33 @@ const Input = () => {
     )
   }
 
-
   const handleChangeKeyUp = evt => {
+    console.log('handleChangeKeyUp', {isBackspace}, evt.keyCode, evt.target.value)
     
-    console.log('handleChangeKeyUp', {inputValue}, {number})
     if (isBackspace) return false;
     setNumber(
       (countDots(evt) == 0 || isBackspace)
         ? evt.target.value
+        // ? ( evt.keyCode == 8 )
+        //     ? evt.target.value.slice(0, -1)
+        //     : evt.target.value
         : replaceDots(evt)
     )
   }
 
   const handleChange = evt => {
-
-    console.log('handleChange', {inputValue}, {number})
     console.log('change:', evt.keyCode, evt.target.value)
     console.log({isBackspace}, evt.keyCode)
     if ( isBackspace ){
       isBackspace = false;
-      return setNumber(evt.target.value);
+      return setNumber(evt.target.value.replace(/[a-zA-Z]|,/gim, ''));
     }
 
-    if (inputValue == "") {
-      inputValue = evt.target.value.replace(/[a-zA-Z]|,/gim, '')
-      setNumber(inputValue)
-      console.log('handleChange', {inputValue}, {number})
-    }
-
-    else {
-      setNumber(
-        (/[a-zA-Z]|,/gim.test(evt.target.value))
-          ? evt.target.value.replace(/[a-zA-Z]|,/gim, '')
-          : evt.target.value.slice(0, -1)
-      )
-
-    }
+    setNumber(
+      (/[a-zA-Z]|,/gim.test(evt.target.value))
+        ? evt.target.value.replace(/[a-zA-Z]|,/gim, '')
+        : evt.target.value.slice(0, -1)
+    )
   }
   
   
