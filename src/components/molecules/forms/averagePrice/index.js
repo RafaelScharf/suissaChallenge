ea4d1d4df975785e5dae73a1e7e0039257b389d6
Suissa <jnascimento@gmail.com>
2020-10-30 16:23:36 -0300

    corrigido o AVG

--- src/components/molecules/forms/averagePrice/index.js
+++ src/components/molecules/forms/averagePrice/index.js
@@ -5,27 +5,30 @@ import Button from '../../../atoms/button/index'
 
 const AvaragePrice = () => {
 
-  const [leverage, setLeverage] = useState('')
-  const [entry, setEntry] = useState('')
-  const [exit, setExit] = useState('')
-  const [quantity, setQuantity] = useState('')
+  const [entry1, setEntry1] = useState('')
+  const [quantity1, setQuantity1] = useState('')
+  const [entry2, setEntry2] = useState('')
+  const [quantity2, setQuantity2] = useState('')
   const [result, setResult] = useState('')
 
-  const calculate = (leverage, entry, exit) => {
+  
+  const mpSUM = ({entry1= 10000, quantity1= 2}, {entry2= 9000, quantity2= 1}) => (
+    (Number(entry1 * quantity1) + Number(entry2 * quantity2)) / 
+    (Number(quantity1) + Number(quantity2))
+  )
 
-    setResult(leverage + entry + exit)
-
-
+  const calculate =  ({entry1= 10000, quantity1= 2}, {entry2= 9000, quantity2= 1}) => {
+    setResult(mpSUM({entry1, quantity1}, {entry2, quantity2}))
   }
 
   return(
 
     <>
-    <Input label="leverage" childHandleChange={(val) => setLeverage(val)}/>
-    <Input label="entry $" childHandleChange={(val) => setEntry(val)} />
-    <Input label="exit $" childHandleChange={(val) => setExit(val)} />
-    <Input label="quantity" childHandleChange={(val) => setLeverage(val)} />
-    <Button text="Calculate" onClick={() => calculate(leverage, entry, exit)} />
+    <Input label="entry1 $" childHandleChange={(val) => setEntry1(val)} />
+    <Input label="quantity1" childHandleChange={(val) => setQuantity1(val)} />
+    <Input label="entry2 $" childHandleChange={(val) => setEntry2(val)} />
+    <Input label="quantity2" childHandleChange={(val) => setQuantity2(val)} />
+    <Button text="Calculate" onClick={() => calculate({entry1, quantity1}, {entry2, quantity2})} />
     {result}
     </>
     
